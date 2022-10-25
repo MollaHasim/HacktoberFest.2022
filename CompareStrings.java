dfed35096fbef699397efff95d0d0026fa7daf27
Molla Hasim <72904271+MollaHasim@users.noreply.github.com>
2022-10-25 12:54:07 +0530

    Create CompareStrings.java

---
+++ CompareStrings.java
@@ -0,0 +1,14 @@
+//How to compare 2 string in java Program
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String style = "Bold";
+        String style2 = "Bold";
+
+        if(style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
